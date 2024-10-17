359c631fcb9209870bda7605cb52f2f0ef6b2c81
Philip Magnus <philip.magnus@ish.de>
2024-10-17 20:44:48 +0200

    Added instantiation of gotils.

--- gotils.go
+++ gotils.go
@@ -1,5 +1,12 @@
 package gotils
 
+type Gotils struct{}
+
+// NewCalculator create a new instance of calculator
+func NewGotils() *Gotils {
+	return &Gotils{}
+}
+
 func reduce[T, M any](s []T, f func(M, T) M, initialValue M) M {
 	accumulator := initialValue
 	for _, v := range s {
